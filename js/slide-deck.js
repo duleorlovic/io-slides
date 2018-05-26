8c6461cc834e8be96e6d6ab0720e7f55f25f38b3
Dusan Orlovic <duleorlovic@gmail.com>
2018-05-26 13:36:40 +0300

    Update slides for Balkan Ruby

--- js/slide-deck.js
+++ js/slide-deck.js
@@ -343,12 +343,12 @@ SlideDeck.prototype.loadConfig_ = function(config) {
       }
       html = presenterTitle.join(' - ') + '<br>';
 
-      var gplus = p.gplus ? '<span>g+</span><a href="' + p.gplus +
+      var gplus = p.gplus ? '<span>user</span><a href="' + p.gplus +
           '">' + p.gplus.replace(/https?:\/\//, '') + '</a>' : '';
 
-      var twitter = p.twitter ? '<span>twitter</span>' +
-          '<a href="http://twitter.com/' + p.twitter + '">' +
-          p.twitter + '</a>' : '';
+      // var twitter = p.twitter ? '<span>twitter</span>' +
+      //     '<a href="http://twitter.com/' + p.twitter + '">' +
+      //     p.twitter + '</a>' : '';
 
       var www = p.www ? '<span>www</span><a href="' + p.www +
                         '">' + p.www.replace(/https?:\/\//, '') + '</a>' : '';
@@ -356,7 +356,7 @@ SlideDeck.prototype.loadConfig_ = function(config) {
       var github = p.github ? '<span>github</span><a href="' + p.github +
           '">' + p.github.replace(/https?:\/\//, '') + '</a>' : '';
 
-      var html2 = [gplus, twitter, www, github].join('<br>');
+      var html2 = [gplus, www, github].join('<br>');
 
       if (dataConfigContact) {
         dataConfigContact.innerHTML = html2;
--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Stop using Alt and Shift<br>when you do not need it',
+    subtitle: 'Keyboard shortcut combinations',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Dusan Orlovic',
+    company: 'Full on Rails developer',
+    gplus: 'duleorlovic',
+    twitter: '',
+    www: 'http://trk.in.rs',
+    github: 'http://github.com/duleorlovic'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
